3ffaa5b26032201bc36e1f7f4ee5a0ab26f8844e
Zachary Burton <45068978+burtonzr@users.noreply.github.com>
2019-03-12 15:39:20 -0500

    Create homepage.php

---
+++ homepage.php
@@ -0,0 +1,9 @@
+<div class="artImage pb-4 mt-4" id="art">
+  <h1 class="title">Artwork</h1>
+</div>
+<div class="friendImage mt-4" id="friend">
+  <h1 class="title">Friends</h1>
+</div>
+<div class="newsfeedImage mt-4" id="newsfeed">
+  <h1 class="title">Newsfeed</h1>
+</div>
